b1f24a24c3dc779d746574fafaff333e74d55417
Caldremch <caldremch@163.com>
2021-01-03 10:03:04 +0800

    reupload-entry-compiler

--- entry-compiler/build.gradle.kts
+++ entry-compiler/build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
 }
 
 ext {
+    this["libraryVersion"] = "1.0.1"
+    this["libraryName"] = "entry-compiler"
     this["myLibraryVersion"] = "1.6.0"
     this["myBintrayName"] = "entry-compiler"
     this["myArtifactId"] = "entry-compiler"
@@ -16,7 +18,7 @@ ext {
 dependencies {
      val javapoet = "com.squareup:javapoet:1.10.0"
     implementation(javapoet)
-    implementation("com.caldremch.android:entry-annotation:1.6.0")
+    implementation("com.caldremch.android:entry-annotation:1.3.0")
     implementation(kotlin("stdlib-jdk8"))
 }
 
@@ -29,8 +31,20 @@ tasks {
     }
 }
 
-
+//val localFile = rootProject.projectDir.resolve("local.properties")
+//val properties = java.util.Properties()
+//properties.load(localFile.inputStream())
+//val myUserName = properties.getProperty("my.username")
+//val myPassword = properties.getProperty("my.password")
+//val myUrl = properties.getProperty("upload.url")
 repositories {
+//    maven {
+//        credentials {
+//            username  = myUserName
+//            password  = myPassword
+//        }
+//        url  = uri(myUrl)
+//    }
     mavenCentral()
 }
 val compileKotlin: KotlinCompile by tasks
@@ -42,4 +56,5 @@ compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
 
-apply(from = "https://raw.githubusercontent.com/caldremch/gradle-maven-kotlin-dsl/master/bintray-with-maven-publish.gradle")
+//apply(from = "https://raw.githubusercontent.com/caldremch/gradle-maven-kotlin-dsl/master/bintray-with-maven-publish.gradle")
+apply(from = "../upload_aar2.gradle")
