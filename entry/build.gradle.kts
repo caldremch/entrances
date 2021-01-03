84adb83c78d671e3bf9ad4f8fe9eb3d0aa2df6e8
Caldremch <caldremch@163.com>
2021-01-03 10:17:57 +0800

    entry-reupload

--- entry/build.gradle.kts
+++ entry/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 
 ext {
     this["myLibraryVersion"] = "1.6.1"
+    this["libraryVersion"] = "1.0.0"
+    this["libraryName"] = "entry"
     this["myBintrayName"] = "entry"
     this["myArtifactId"] = "entry"
     this["myLibraryName"] = "entry for list"
@@ -25,10 +27,11 @@ android {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("androidx.appcompat:appcompat:1.2.0")
-    api("com.caldremch.android:entry-annotation:1.6.0")
+    api("com.caldremch.android:entry-annotation:1.3.0")
     api(
         "androidx.annotation:annotation:1.1.0"
     )
 }
+apply(from = "../upload_aar2.gradle")
 
-apply(from = "https://raw.githubusercontent.com/caldremch/gradle-maven-kotlin-dsl/master/bintray-with-maven-publish.gradle")
+//apply(from = "https://raw.githubusercontent.com/caldremch/gradle-maven-kotlin-dsl/master/bintray-with-maven-publish.gradle")
